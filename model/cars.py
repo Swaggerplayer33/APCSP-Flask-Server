957c41a70eea8925aac3379fc59f53fe19287306
Swaggeplayer33 <fifteentanav@email.com>
2023-10-15 22:07:51 -0700

    cars api = good to go

--- model/cars.py
+++ model/cars.py
@@ -1,18 +1,34 @@
 import requests
 
-url = "rapidapi.com"
+url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars"
+
+modelInput = str(input("What is your car model: "))
+querystring = {"model": modelInput}
 
 headers = {
-    'X-RapidAPI-Key: a8491de794msh6676acc5521c4fcp1c5cf8jsn6d99a8656992'
-    'X-RapidAPI-Host: cars-by-api-ninjas.p.rapidapi.com'
-} 
+	"X-RapidAPI-Key": "a8491de794msh6676acc5521c4fcp1c5cf8jsn6d99a8656992",
+	"X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com"
+}
 
-import requests
+response = requests.get(url, headers=headers, params=querystring)
 
-model = 'camry'
-api_url = 'https://api.api-ninjas.com/v1/cars?model={}'.format(model)
-response = requests.get(api_url, headers={'X-Api-Key': 'YOUR_API_KEY'})
-if response.status_code == requests.codes.ok:
-    print(response.text)
-else:
-    print("Error:", response.status_code, response.text)
+print(response.json())
+
+
+
+#import requests
+
+#url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars"
+
+#headers = {
+#    'X-RapidAPI-Key: a8491de794msh6676acc5521c4fcp1c5cf8jsn6d99a8656992'
+#    'X-RapidAPI-Host: cars-by-api-ninjas.p.rapidapi.com'
+#} 
+
+#model = 'camry'
+#api_url = 'https://api.api-ninjas.com/v1/cars?model={}'.format(model)
+#response = requests.get(api_url, headers={'X-Api-Key': 'YOUR_API_KEY'})
+#if response.status_code == requests.codes.ok:
+#    print(response.text)
+#else:
+#    print("Error:", response.status_code, response.text)
