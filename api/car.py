2b76a81dd6b9d17a6eff77e05cd8fdb42ca2ed13
Swaggeplayer33 <fifteentanav@email.com>
2023-10-24 20:21:01 -0700

    own api

---
+++ api/car.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, jsonify
+from flask_restful import Api, Resource, reqparse
+from __init__ import app, db
+from model.ownCarsAPI import Cars
+
+car_api = Blueprint('car', __name__, url_prefix='/api/car')
+api = Api(car_api)
+
+class CarsResource(Resource):
+    def get(self):
+        cars = Cars.query.all()
+        car_list = [car.alldetails() for car in cars]
+        return jsonify(car_list)
+
+class CarDetailsResource(Resource):
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('id', type=int, required=True)
+        super(CarDetailsResource, self).__init__()
+
+    def get(self):
+        args = self.parser.parse_args()
+        car = Cars.query.get(args['id'])
+        if car is None:
+            return jsonify({"error": "Car not found"}), 404
+        return jsonify(car.alldetails())
+
+api.add_resource(CarsResource, "/cars")
+api.add_resource(CarDetailsResource, "/cardetails")
--- model/ownCarsAPI.py
+++ model/ownCarsAPI.py
@@ -1,49 +1,68 @@
-
+from __init__ import app, db
+from flask_login import UserMixin
+from sqlalchemy.exc import IntegrityError
 import os
-import csv
-from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
 
-app = Flask(__name)
-
-
-class Car(db.Model):
+# Car model
+class Cars(db.Model, UserMixin):
     __tablename__ = 'cars'
 
     id = db.Column(db.Integer, primary_key=True)
-    make = db.Column(db.String(50), nullable=False)
-    model = db.Column(db.String(50), nullable=False)
-    year = db.Column(db.Integer, nullable=False)
-    trim = dbm.Column(db.Float, nullable=False)
+    make = db.Column(db.String(128), unique=True, nullable=False)
+    model = db.Column(db.String(256), nullable=False)
+    year = db.Column(db.String(256), nullable=False)  # Assuming instructions can be nullable
+    trim = db.Column(db.String(64), nullable=False)  # Assuming image_name can be nullable
+    cylinders = db.Column(db.String(256), nullable=False)  # Assuming cleaned_ingredients can be nullable
 
+    def __init__(self, make, model, year, trim, cylinders):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.trim = trim
+        self.cylinders = cylinders
+
+    def alldetails(self):
+        return {
+            "id": self.id,
+            "make": self.make,
+            "model": self.model,
+            "year": self.year,
+            "trim": self.trim,
+            "cylinders": self.cylinders
+        }
+
+# Favorite mode
+# Function to initialize recipes
 def initCars():
     with app.app_context():
-        print("Creating car tables")
+        print("Loading Cars")
         db.create_all()
-        if db.session.query(Car).count() > 0:
+        if db.session.query(Cars).count() > 0:
             return
 
         basedir = os.path.abspath(os.path.dirname(__file__))
-        file_path = os.path.join(basedir, "/Flask-Server-Back-End/CarStatsDatabase.csv")
+        file_path = os.path.join(basedir, "/Flask-Server-Back-End/CarStats.csv")  # Changed to use os.path.join for better compatibility
+        df = pd.read_csv(file_path)
 
-        with open(file_path, 'r') as csv_file:
-            csv_reader = csv.DictReader(csv_file)
-            for row in csv_reader:
-                car = Car(
-                    make=row['Make'],
-                    model=row['Model'],
-                    year=int(row['Year']),
-                    trim=float(row['Trim'])
-                )
+        for index, row in df.iterrows():
+            car = Cars(
+                make=row['Make'],
+                model=row['Model'],
+                year=row.get('Year', None),  # Added a get method to handle the possibility of the key not existing
+                trim=row.get('trim', None),
+                cylinders=row.get('cylinders', None)
+            )
 
-                db.session.add(car)
-                try:
-                    db.session.commit()
-                except IntegrityError:
-                    db.session.rollback()
-                    print(f"Duplicate car or error: {car.make} {car.model}")
-                except Exception as e:
-                    db.session.rollback()
-                    print(f"Error adding car: {str(e)}")
+            db.session.add(car)
+            try:
+                db.session.commit()
+            except IntegrityError:
+                db.session.rollback()
+                print(f"Duplicate car or error: {Cars.title}")
+            except Exception as e:
+                db.session.rollback()
+                print(f"Error adding car at index {index}: {str(e)}")
 
 if __name__ == "__main__":
     initCars()
