4e9b110eba88e5284f695fa41bb26450f26c2cd3
ad1tyad3sa1 <aditya7desai@gmail.com>
2023-10-24 20:33:21 -0700

    AdityaD and Tanav Backend

--- model/cars.py
+++ model/cars.py
@@ -1,34 +1,68 @@
-import requests
+from __init__ import app, db
+from flask_login import UserMixin
+from sqlalchemy.exc import IntegrityError
+import os
+import pandas as pd
 
-url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars"
+# Car model
+class Cars(db.Model, UserMixin):
+    __tablename__ = 'cars'
 
-modelInput = str(input("What is your car model: "))
-querystring = {"model": modelInput}
+    id = db.Column(db.Integer, primary_key=True)
+    make = db.Column(db.String(128), unique=True, nullable=False)
+    model = db.Column(db.String(256), nullable=False)
+    year = db.Column(db.String(256), nullable=False)  # Assuming instructions can be nullable
+    trim = db.Column(db.String(64), nullable=False)  # Assuming image_name can be nullable
+    cylinders = db.Column(db.String(256), nullable=False)  # Assuming cleaned_ingredients can be nullable
 
-headers = {
-	"X-RapidAPI-Key": "a8491de794msh6676acc5521c4fcp1c5cf8jsn6d99a8656992",
-	"X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com"
-}
+    def __init__(self, make, model, year, trim, cylinders):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.trim = trim
+        self.cylinders = cylinders
 
+    def alldetails(self):
+        return {
+            "id": self.id,
+            "make": self.make,
+            "model": self.model,
+            "year": self.year,
+            "trim": self.trim,
+            "cylinders": self.cylinders
+        }
 
-def responses():
-    response = requests.get(url, headers=headers, params=querystring)
-    return response
+# Favorite mode
+# Function to initialize recipes
+def initCars():
+    with app.app_context():
+        print("Loading Cars")
+        db.create_all()
+        if db.session.query(Cars).count() > 0:
+            return
 
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        file_path = os.path.join(basedir, "/Flask-Server-Back-End/CarStats.csv")  # Changed to use os.path.join for better compatibility
+        df = pd.read_csv(file_path)
 
-#import requests
+        for index, row in df.iterrows():
+            car = Cars(
+                make=row['Make'],
+                model=row['Model'],
+                year=row.get('Year', None),  # Added a get method to handle the possibility of the key not existing
+                trim=row.get('trim', None),
+                cylinders=row.get('cylinders', None)
+            )
 
-#url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars"
+            db.session.add(car)
+            try:
+                db.session.commit()
+            except IntegrityError:
+                db.session.rollback()
+                print(f"Duplicate car or error: {Cars.title}")
+            except Exception as e:
+                db.session.rollback()
+                print(f"Error adding car at index {index}: {str(e)}")
 
-#headers = {
-#    'X-RapidAPI-Key: a8491de794msh6676acc5521c4fcp1c5cf8jsn6d99a8656992'
-#    'X-RapidAPI-Host: cars-by-api-ninjas.p.rapidapi.com'
-#} 
-
-#model = 'camry'
-#api_url = 'https://api.api-ninjas.com/v1/cars?model={}'.format(model)
-#response = requests.get(api_url, headers={'X-Api-Key': 'YOUR_API_KEY'})
-#if response.status_code == requests.codes.ok:
-#    print(response.text)
-#else:
-#    print("Error:", response.status_code, response.text)
+if __name__ == "__main__":
+    initCars()
