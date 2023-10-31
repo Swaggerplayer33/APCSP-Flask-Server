96b023414ffe27abd30ca2d08a7fe5716b42e916
ad1tyad3sa1 <aditya7desai@gmail.com>
2023-10-30 22:56:40 -0700

    email api fix

--- api/email.py
+++ api/email.py
@@ -1,41 +1,51 @@
+from flask import Flask, Blueprint, request
+from flask_restful import Api  # Import Api from Flask-RESTful (if you want to use it)
 import random
 import string
-
-random_code = ''.join(random.choices(string.digits, k=6))
-
-from flask import Flask, request
-import random
-import string
-import os
 import sendgrid
 from sendgrid.helpers.mail import Mail
 
-# Generate a random code
-def generate_random_code():
-    return ''.join(random.choices(string.digits, k=6))
+# Create a Flask app instance
+app = Flask(__name__)
 
-# Process the form and send the email
-@app.route('/submit', methods=['POST'])
-def submit_form():
-    user = request.form['user']
-    email = request.form['email']
+# Create a Blueprint for the email API
+email_api = Blueprint('email_api', __name__, url_prefix='/api/email')
 
-    random_code = generate_random_code()
-    message = f"Hello {user}, your code is {random_code}. Show this code when you checkout your car."
+# Create the API instance (if you want to use Flask-RESTful)
+api = Api(email_api)
+
+# Define the send_email function
+def send_email(user, email):
+    # Generate a random code
+    random_code = ''.join(random.choices(string.digits, k=6))
+
+    # Construct the email message
+    message = Mail(
+        from_email="aditya7desai@gmail.com",
+        to_emails=email,
+        subject="Car Checkout Code",
+        plain_text_content=f"Hello {user}, your code is {random_code}. Show this code when you checkout your car."
+    )
 
     # Send the email using SendGrid
-    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('08d7639959msh856cfb5944a18fap147283jsnf269b28df190'))
-    from_email = sendgrid.helpers.mail.Email("aditya7desai@gmail.com")
-    to_email = sendgrid.helpers.mail.Email(email)
-    subject = "Your Car Checkout Code"
-    content = sendgrid.helpers.mail.Content("text/plain", message)
-    mail = sendgrid.helpers.mail.Mail(from_email, subject, to_email, content)
-    
-    try:
-        response = sg.client.mail.send.post(request_body=mail.get())
-        return "Email sent successfully."
-    except Exception as e:
-        return f"Email sending failed: {str(e)}"
+    sg = sendgrid.SendGridAPIClient(api_key="YOUR_SENDGRID_API_KEY")
+    sg.send(message)
 
+# Define a route for form submission
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    if request.method == 'POST':
+        user = request.form['user']
+        email = request.form['email']
+
+        # Check if user and email are provided
+        if user and email:
+            send_email(user, email)
+            return "Email sent successfully."
+        else:
+            return "User and email are required."
+
+# Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+
