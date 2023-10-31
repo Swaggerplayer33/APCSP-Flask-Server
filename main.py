f93dff9dd98501f43fca7791477604c8a8edd2ae
ad1tyad3sa1 <aditya7desai@gmail.com>
2023-10-31 10:39:01 -0700

    fixing errors

--- main.py
+++ main.py
@@ -7,15 +7,12 @@ from flask import render_template  # import render_template from "public" flask 
 from __init__ import app,db  # Definitions initialization
 from model.jokes import initJokes
 from model.users import initUsers
-from model.players import initPlayers
 from model.cars import initCars
 
 
 # setup APIs
 from api.covid import covid_api # Blueprint import api definition
 from api.joke import joke_api # Blueprint import api definition
-from api.user import user_api # Blueprint import api definition
-from api.player import player_api
 from api.car import car_api 
 
 
@@ -29,8 +26,6 @@ db.init_app(app)
 # register URIs
 app.register_blueprint(joke_api) # register api routes
 app.register_blueprint(covid_api) # register api routes
-app.register_blueprint(user_api) # register api routes
-app.register_blueprint(player_api)
 app.register_blueprint(app_projects) # register app pages
 app.register_blueprint(car_api) #register car api
 
@@ -53,7 +48,6 @@ def activate_job(): # activate these items
     db.create_all() #recreates api entries
     initJokes()
     initUsers()
-    initPlayers()
     initCars()
 
 # this runs the application on the development server
